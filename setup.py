0114a5ad04ba50736204990162bc110018dec7d2
vipermu <vipermu97@gmail.com>
2021-12-22 23:26:03 +0100

    feat: add setup and update requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    requirements_list = req_file.read().strip().split("\n")
+
+setuptools.setup(
+    name="clip",
+    packages=["clip"],
+    install_requires=requirements_list,
+)
